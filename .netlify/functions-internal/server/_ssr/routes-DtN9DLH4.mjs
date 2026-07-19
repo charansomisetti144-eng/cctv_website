8110146af66f436337d3f646ffed3c60bb2664a8
Charan <charansomisetti144@gmail.com>
2026-07-19 14:50:12 +0530

    configure nitro to netlify

---
+++ .netlify/functions-internal/server/_ssr/routes-DtN9DLH4.mjs
@@ -0,0 +1,7 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/routes-DtN9DLH4.js
+function Index() {
+	if (typeof window !== "undefined") window.location.replace("/index.html");
+	return null;
+}
+//#endregion
+export { Index as component };
---
+++ .netlify/functions-internal/server/_tanstack-start-manifest_v-DaaRAjNq.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DaaRAjNq.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/somis/Documents/learnings/PROJECTS/cctv_website/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-BgyNRdUb.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-BgyNRdUb.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/somis/Documents/learnings/PROJECTS/cctv_website/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-yq8oUfn7.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
